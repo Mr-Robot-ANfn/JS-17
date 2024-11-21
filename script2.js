8f3e50c2b1f5219a68a98e8fba0e1063f4aa1388
Shahab-Saeed <shahabsaeed888@gmail.com>
2024-11-21 17:24:36 +0500

    a

---
+++ script2.js
@@ -0,0 +1,19 @@
+//QUESTION 1
+
+// let a=document.querySelector(".link").addEventListener("click",()=>{
+//     alert("you are heading towards google")
+// })
+
+
+//QUESTION 2
+
+// let b=document.querySelector(".c    ").addEventListener("click",()=>{
+//     alert("Thanks! for purchasing a phone from us")
+// })
+
+
+//QUESTION 3
+
+// function deleteRow(button){
+//     button.parentElement.parentElement.remove();
+// }
